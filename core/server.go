fbfde0665a75f9e782ce6d46318e9f17d56e78a4
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-03-04 22:45:32 +0800

    feat: 初始化

--- core/server.go
+++ core/server.go
@@ -1,5 +1,19 @@
 package core
 
-func RunServer() {
+import (
+	"github.com/afl-lxw/gin-trend/initialize"
+	"time"
+)
 
+type server interface {
+	ListenAndServe() error
+}
+
+func RunServer() {
+	Router := initialize.Routers()
+	//address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
+	s := initServer("8888", Router)
+	time.Sleep(10 * time.Microsecond)
+
+	s.ListenAndServe()
 }
