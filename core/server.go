74655f6d117d742669d0fcbd0d53c656815b9abc
af-liu <18264480900@163.com>
2024-03-06 15:18:19 +0800

    feat: 项目初始化zap

--- core/server.go
+++ core/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/afl-lxw/gin-trend/global"
 	"github.com/afl-lxw/gin-trend/initialize"
+	"go.uber.org/zap"
 	"time"
 )
 
@@ -17,9 +18,9 @@ func RunServer() {
 	address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
 	s := initServer(address, Router)
 	time.Sleep(10 * time.Microsecond)
-	fmt.Printf("开始运行 运行地址为 http://127.0.0.1%s", address)
+	fmt.Printf("开始运行 运行地址为 http://127.0.0.1%s \n", address)
 
-	//global.GVA_LOG.Info("server run success on ", zap.String("address", address))
+	global.GVA_LOG.Info("server run success on ", zap.String("address", address))
 	fmt.Printf(`默认自动化文档地址:http://127.0.0.1%s/swagger/index.html`, address)
 	global.GVA_LOG.Error(s.ListenAndServe().Error())
 }
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	fmt.Println("开始执行==》")
 	global.GVA_VP = core.Viper() // 初始化Viper
+	global.GVA_LOG = core.Zap()  // 初始化zap日志库
 
 	core.RunServer()
 }
