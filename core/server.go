8a6eda16489e5895962f8a4fc3166d35b27506f9
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-03-11 23:53:36 +0800

    feat: 初始化

--- core/server.go
+++ core/server.go
@@ -29,13 +29,20 @@ func RunServer() {
 	//	system.LoadAll()
 	//}
 	Router := initialize.Routers()
+
+	Router.Static("/form-generator", "./resource/page")
+
 	//address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
 	address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
 	s := initServer(address, Router)
+
 	time.Sleep(10 * time.Microsecond)
 	fmt.Println(banner.Inline("welcome to GO Trend."))
-	fmt.Printf("开始运行 运行地址为 http://127.0.0.1%s \n", address)
 	global.GVA_LOG.Info("server run success on ", zap.String("address", address))
-	fmt.Printf(`默认自动化文档地址:http://127.0.0.1%s/swagger/index.html`, address)
+	fmt.Printf(`
+	默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
+	开始运行 运行地址为 http://127.0.0.1%s 
+	`,
+		address, address)
 	global.GVA_LOG.Error(s.ListenAndServe().Error())
 }
