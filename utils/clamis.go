03018edfb1dd19baaf47299a858bbb7cd83f6521
liuxiaowen <liuxiaowen66621@gmail.com>
2024-03-15 18:35:01 +0800

    feat: Jwt V5

--- utils/clamis.go
+++ utils/clamis.go
@@ -4,9 +4,49 @@ import (
 	//"github.com/afl-lxw/gin-trend/global"
 	//systemReq "github.com/afl-lxw/gin-trend/model/system/request"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"os"
+
 	//"github.com/gofrs/uuid/v5"
 	"net"
 )
+
+type UserClaims struct {
+	Id    string `json:"id"`
+	First string `json:"first"`
+	Last  string `json:"last"`
+	jwt.RegisteredClaims
+}
+
+func NewAccessToken(claims UserClaims) (string, error) {
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return accessToken.SignedString([]byte(os.Getenv("TOKEN_SECRET")))
+}
+
+func NewRefreshToken(claims jwt.RegisteredClaims) (string, error) {
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return refreshToken.SignedString([]byte(os.Getenv("TOKEN_SECRET")))
+}
+
+func ParseAccessToken(accessToken string) *UserClaims {
+	parsedAccessToken, _ := jwt.ParseWithClaims(accessToken, &UserClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("TOKEN_SECRET")), nil
+	})
+
+	return parsedAccessToken.Claims.(*UserClaims)
+}
+
+func ParseRefreshToken(refreshToken string) *jwt.RegisteredClaims {
+	parsedRefreshToken, _ := jwt.ParseWithClaims(refreshToken, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("TOKEN_SECRET")), nil
+	})
+
+	return parsedRefreshToken.Claims.(*jwt.RegisteredClaims)
+}
+
+// ------------------------------------------------------------------------------------------------------
 
 func ClearToken(c *gin.Context) {
 	// 增加cookie x-token 向来源的web添加
---
+++ utils/jwt.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/afl-lxw/gin-trend/global"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+func createToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username": username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString(global.TREND_CONFIG.JWT.SigningKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return global.TREND_CONFIG.JWT.SigningKey, nil
+	})
+	if err != nil {
+		return err
+	}
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+	return nil
+}
