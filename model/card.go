a378c21edcb5196e51aa7f2d61e3a1db772b34cb
liuxiaowen <liuxiaowen66621@gmail.com>
2024-03-26 20:51:49 +0800

    feat: card

--- model/card.go
+++ model/card.go
@@ -1,4 +1,17 @@
 package model
 
+import (
+	"github.com/afl-lxw/gin-trend/global"
+	"github.com/segmentio/ksuid"
+)
+
 type UserCard struct {
+	global.TREND_MODEL
+	UUID     ksuid.KSUID `json:"uuid" gorm:"type:varchar(255);index;comment:用户UUID"`
+	Title    string      `json:"title" gorm:"comment:标题"`
+	Content  string      `json:"content" gorm:"comment:内容"`
+	ImgList  []string    `json:"imgList" gorm:"comment:图片列表"`
+	Address  string      `json:"address" gorm:"comment:地址"`
+	IsDelete bool        `json:"isDelete" gorm:"comment:是否删除"`
+	IsHot    bool        `json:"isHot" gorm:"comment:推荐热门"`
 }
