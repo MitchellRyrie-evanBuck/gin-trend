5f055011d8db5feb5871add4c7d7aa05f9522e92
liuxiaowen <liuxiaowen66621@gmail.com>
2024-05-10 23:29:24 +0800

    feat: TAGS API

--- model/tags.go
+++ model/tags.go
@@ -1,8 +1,12 @@
 package model
 
-import "github.com/afl-lxw/gin-trend/global"
+import (
+	"github.com/afl-lxw/gin-trend/global"
+	"runtime/metrics"
+)
 
 type Tags struct {
 	global.TREND_MODEL
 	Name string
+	metrics.Description
 }
